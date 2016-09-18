418494b9565b413a25f7f565ab610c7956c5acf8
Marc Risney <mrisney@steelhouse.com>
2016-09-17 22:30:32 -0700

    added HelloWorldServlet

---
+++ src/main/java/ai/api/twilio/HelloWorldServlet.java
@@ -0,0 +1,15 @@
+package ai.api.twilio;
+
+import java.io.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.println("Hello, world!");
+		out.close();
+	}
+}
